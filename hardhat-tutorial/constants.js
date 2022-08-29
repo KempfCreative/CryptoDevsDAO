48fc1601ab609926ff476c3205b337641d9d77b7
Jonathan Kempf <kempfjj@gmail.com>
2022-08-28 22:19:18 -0400

    Initial commit for DAO

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xd9F823D8840F72392Fc4b50f7a7c21627c0E47dB";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
